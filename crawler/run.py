af6a8d5d1237bb7bc752a7c1f2e882ea0af34cdb
re3turn <re3turn@gmail.com>
2022-02-24 15:01:31 +0000

    Add OUTPUT_LOG_FILE_ENABLED config

--- crawler/run.py
+++ crawler/run.py
@@ -10,9 +10,9 @@ def main() -> None:
     crawler.Crawler().main()
 
 
+logger: logging.Logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     Log.init_logger(log_name='run')
-    logger: logging.Logger = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
     main()
-
-logger = logging.getLogger(__name__)
